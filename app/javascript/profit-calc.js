1687b27d5c16855383607572f0d07a3da9b918ce
nzk-tomo <eleten0000@gmail.com>
2020-11-04 23:54:31 +0900

    微修正

--- app/javascript/profit-calc.js
+++ app/javascript/profit-calc.js
@@ -1,9 +1,9 @@
-addEventListener('load', () => {
+addEventListener("turbolinks:load", () => {
   let price = document.getElementById("item-price"),
       tax = document.getElementById("add-tax-price"),
       profit = document.getElementById("profit")
   price.addEventListener("input",() => {
-    e = document.getElementById("item-price").value.replace( /[０-９]/g,function(s){
+    e = price.value.replace( /[０-９]/g,function(s){
       return String.fromCharCode(s.charCodeAt(0) - 0xFEE0)});
     tax.innerHTML = Math.floor(e * 0.1).toLocaleString()
     profit.innerHTML = Math.floor(e * 0.9).toLocaleString()
