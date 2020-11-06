fada836b774c01731a57124fb8ff6b3fb82e4689
nzk-tomo <eleten0000@gmail.com>
2020-11-07 01:42:46 +0900

    payjp導入1

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,13 +1,16 @@
 class TradesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @item_trade = ItemTrade.new(trade_params) 
-
-    if @item_trade.save
-      redirect_to root_path
+    if @item_trade.valid?
+      pay_item
+      @item_trade.save
+      return redirect_to root_path
     else
       render :index
     end
@@ -16,10 +19,15 @@ class TradesController < ApplicationController
   private
 
   def trade_params
-    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id).merge(user_id: current_user.id)
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token).merge(user_id: current_user.id, token: params[:token])
   end
 
-  # def address_params(trade)
-  #   params.permit(:postal_code, :prefecture, :city, :house_number, :building_name).merge(trade_id: trade.id)
-  # end
+  def pay_item
+    Payjp.api_key = "テスト秘密鍵"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: @item_trade.token,
+      currency: 'jpy'
+    )
+  end
 end
