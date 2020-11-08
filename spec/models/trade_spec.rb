8014fa3b12732927ba1dbc2e492914afb4618d32
nzk-tomo <eleten0000@gmail.com>
2020-11-08 23:51:38 +0900

    テストコード実装

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,27 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe ItemTrade, type: :model do
-  describe '#create' do
-    before do
-      @item_trade = FactoryBot.build(:item_trade)
-    end
-  
-
-    describe '商品購入' do
-      context '商品購入がうまくいくとき' do
-        it "priceがあれば保存ができること" do
-          expect(@order).to be_valid
-        end
-      end
-    
-      context '商品出品がうまくいかないとき' do
-        it "tokenが空では保存ができないこと" do
-          @order.price = nil
-          @order.valid?
-          expect(@order.errors.full_messages).to include("Price can't be blank")
-        end
-        
-      end
-    end
-  end
+RSpec.describe Trade, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
